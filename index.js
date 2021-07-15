c671fd9a4c3875c26a4ff98e55ca1edde5ac894e
marcosATr <marcos.trannin@gmail.com>
2021-07-15 16:54:48 -0300

    error

--- index.js
+++ index.js
@@ -1,54 +1,11 @@
 const express = require("express");
+const routesFF = require("./routes/routes.js");
 const app = express();
-port = 4000;
+port = 3000;
 
 app.use(express.json());
 
-const skyscrapers = [
-  "Burj Khalifa",
-  "Shanghai Tower",
-  "Abraj Al Bait",
-  "ing An Finance Centre",
-  "Lotte World Tower",
-];
-
-app.get("/", (req, res) => {
-  res.send("Welcome");
-});
-
-app.get("/skyscraper", (req, res) => {
-  res.send(skyscrapers);
-});
-
-app.get("/skyscraper/:id", (req, res) => {
-  const id = req.params.id - 1;
-  const skyscraper = skyscrapers[id];
-  if (!skyscraper) {
-    return res.status(404).send("deu ruim!");
-  }
-  res.send(skyscraper);
-});
-
-//parte 2
-
-app.post("/skyscraper", (req, res) => {
-  const skyscraper = req.body.skyscraper;
-  const id = skyscrapers.length;
-  skyscrapers.push(skyscraper);
-});
-
-//put // /filmes/id
-app.put("/skyscraper/:id", (req, res) => {
-  const id = req.params.id - 1;
-  const skyscraper = req.body.skyscraper;
-  skyscrapers[id] = skyscraper;
-  res.send(`${skyscraper} atualizado.`);
-});
-
-app.delete("/skyscraper/:id", (req, res) => {
-  const id = req.params.id - 1;
-  delete skyscrapers[id];
-});
+app.use("/", routesFF);
 
 app.listen(port, () => {
   console.info(`App rodando em: http://localhost:${port}`);
